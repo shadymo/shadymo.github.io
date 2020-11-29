9ea212908ac059798ca18fb68ae90874692b15e9
Apple <apple@As-MacBook-Pro.local>
2020-11-28 19:09:38 -0500

    added new songs, added shuffle feature, fixed bug where next song wouldnt work after a selected song

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const songs = [ 'aston.mp3', 'connections.mp3', 'high.m4a', 'mfeature.mp3', 'Tactical.mp3', 'Tired of this shit.mp3']
+const songs = ['80Way.mp3', '2020 Vision.mp3', 'Aston.mp3', 'Connections.mp3',
+              'Flamingo.mp3', 'Go Get It.mp3', 'High.m4a', 'HitList.mp3',
+              'Level Up.mp3', 'mfeature.mp3', 'No Hook Part 2.mp3', 'Pop A Ten.mp3',
+              'Tactical.mp3', 'Talk To Em.mp3', 'Tired of this Shit.mp3', 'We Dont Do Baccardi.mp3'];
 
 
 const music = document.getElementById('music');
 let counter = 0;
+let isShuffleOn = false;
 
 
 
@@ -11,6 +15,7 @@ function play(song){
     if(song){
         music.src = `./songs/${song}`
         document.getElementById('current-song').innerText = convertSongName(song);
+        getCounterFromSong(song)
 
     }else{
         music.src = `./songs/${songs[counter]}`;
@@ -24,13 +29,22 @@ function play(song){
 }
 
 function nextSong(){
+  if(isShuffleOn){
+    let shuffledCounter = Math.floor(Math.random() *songs.length)
+    counter = shuffledCounter;
+    play();
+  }else{
     if(counter < songs.length -1){
         counter +=1
     }else{
         counter = 0
     }
     play();
+  }
+
 }
+
+
 
 function createSongElements(){
     let songHolder = document.getElementById('song-holder');
@@ -39,9 +53,10 @@ function createSongElements(){
         p.innerText = convertSongName(song);
         p.classList.add('songName')
         songHolder.appendChild(p)
-        
     })
 }
+
+
 
 function convertSongName(path){
  let pathArr = path.split('');
@@ -53,25 +68,34 @@ function convertSongName(path){
         return word;
     }
  }
- 
     return word;
 }
 
+function getCounterFromSong(song){
+  for(var i = 0; i < songs.length; i++){
+    if(songs[i] == song){
+      counter = i;
+    }
+  }
+}
 
 
 document.addEventListener('click', (e)=>{
     let target = e.target;
-    
+
     if(target.id == 'start'){
-        document.getElementById('start').remove()
+        document.getElementById('welcome-screen').remove()
         document.getElementById('music-player').style.display = 'flex';
         createSongElements();
         play();
-    }
-    if(target.id == 'next'){
+    }else if(target.id == 'next'){
         nextSong();
+    }else if(target.id == 'shuffle'){
+      isShuffleOn = !isShuffleOn;
+      document.getElementById('shuffle-check').innerText = isShuffleOn;
+      nextSong();
     }
-    
+
     if(target.classList[0] == 'songName'){
         let songPath = ''
         if(target.innerText == 'high'){
@@ -79,10 +103,6 @@ document.addEventListener('click', (e)=>{
         }else{
             songPath = target.innerText + '.mp3'
         }
-       console.log(songPath);
         play(songPath)
-        
     }
-    
 })
-
